200c4f891d9b066383d99108419f24ed596a1f9c
yaoqilin <1392675219@qq.com>
2019-05-31 22:54:47 +0800

    个人详情表单提交

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public peopleInfo:any={
+    username:'',
+    sex:'2',
+    cityList:['北京','上海','深圳'],
+    city:'北京',
+    hobby:[{
+      title:'吃饭',
+      checked:false
+    },{
+      title:'睡觉',
+      checked:false
+    },{
+      title:'打豆豆',
+      checked:true
+    }]
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  submit(): void{debugger;
+    this
+  }
 }
