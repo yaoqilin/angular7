baa738c9a7603b56c9c3ea5904242b05e954983f
yaoqilin <1392675219@qq.com>
2019-06-09 09:55:24 +0800

    添加list组件

--- src/app/pages/main/left-control/left-control.component.ts
+++ src/app/pages/main/left-control/left-control.component.ts
@@ -1,24 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {LocalStorageService} from "../../../services/local-storage.service";
-import {USERNAME} from "../../../services/local-storage.namespace";
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ListComponent } from './list/list.component';
+import { LocalStorageService } from '../../../services/local-storage/local-storage.service';
+import { USERNAME } from '../../../services/local-storage/local-storage.namespace';
 
 @Component({
   selector: 'app-left-control',
   templateUrl: './left-control.component.html',
-  styleUrls: ['./left-control.component.css']
+  styleUrls: [ './left-control.component.less' ]
 })
 export class LeftControlComponent implements OnInit {
-
   @Input() isCollapsed: boolean;
+  @ViewChild(ListComponent) listComponent: ListComponent;
 
   username: string;
 
   constructor(
     private store: LocalStorageService
-  ) {
-  }
+  ) { }
 
   ngOnInit() {
     this.username = this.store.get(USERNAME);
+  }
+
+  openAddListModal(): void {
+    // 在 Angular 中调用子组件
+    this.listComponent.openAddListModal();
   }
 }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { LeftControlComponent } from './left-control/left-control.component';
+import { ListComponent } from './left-control/list/list.component';
 
 @NgModule({
-  declarations: [MainComponent, LeftControlComponent],
+  declarations: [MainComponent, LeftControlComponent, ListComponent],
   imports: [
     CommonModule
   ]
