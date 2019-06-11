be02906950e17f558fc668e02e1584d30c6b7a82
yaoqilin <1392675219@qq.com>
2019-06-11 22:58:36 +0800

    添加右侧内容部分

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { LeftControlComponent } from './left-control/left-control.component';
 import { ListComponent } from './left-control/list/list.component';
+import { RightControlComponent } from './right-control/right-control.component';
+import { HeaderComponent } from './right-control/header/header.component';
+import { QuickAddComponent } from './right-control/quick-add/quick-add.component';
+import { TodoComponent } from './right-control/todo/todo.component';
 
 @NgModule({
-  declarations: [MainComponent, LeftControlComponent, ListComponent],
+  declarations: [MainComponent, LeftControlComponent, ListComponent, RightControlComponent, HeaderComponent, QuickAddComponent, TodoComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/pages/main/right-control/right-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-control',
+  templateUrl: './right-control.component.html',
+  styleUrls: ['./right-control.component.css']
+})
+export class RightControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
