06af6bc31b4c92460398280e95849099d521fe89
yaoqilin <1392675219@qq.com>
2019-06-27 08:03:37 +0800

    添加guard服务

--- src/app/services/init-guard/init-guard.service.ts
+++ src/app/services/init-guard/init-guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {LocalStorageService} from "../local-storage.service";
+import {INIT_FLAG} from "../local-storage.namespace";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class InitGuardService {
 
-  constructor() { }
+@Injectable()
+export class InitGuardService implements CanActivate {
+  constructor(
+    private store: LocalStorageService,
+    private router: Router,
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const init = !!this.store.get(INIT_FLAG);
+
+    if (state.url.includes('setup') && init) {
+      this.router.navigateByUrl('/main');
+      return false;
+    }
+    if (!state.url.includes('setup') && !init) {
+      this.router.navigateByUrl('/setup');
+      return false;
+    }
+
+    return true;
+  }
 }
