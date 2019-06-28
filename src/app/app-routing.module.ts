6bd750e8d6fcc4c14aeab89bb9ab14eeae22c900
yaoqilin <1392675219@qq.com>
2019-06-28 14:27:29 +0800

    添加路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./component/login/login.component";
 import {SetupComponent} from "./pages/setup/setup.component";
+import {InitGuardService} from "./services/init-guard/init-guard.service";
 
 const routes: Routes = [
   {
     path:'setup',
-    component:SetupComponent
+    component:SetupComponent,
+    canActivate: [ InitGuardService ]
   },
   {
     path: 'main',
